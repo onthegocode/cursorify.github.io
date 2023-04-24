201e43113c2fdb6c7b5c5226c0ae66e6363909e0
morethanmin <mini4614@gmail.com>
2023-04-24 23:24:11 +0900

    feat: :sparkles: update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,7 +42,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/cursorify/cursorify.github.io/tree/main/docs',
+            'https://github.com/cursorify/cursorify.github.io/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
